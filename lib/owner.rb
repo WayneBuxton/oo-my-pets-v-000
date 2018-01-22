000a33b29f707e5d811a14451d6b84ed415e18e1
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-22 01:34:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,12 +70,12 @@ class Owner
     pets[:dogs].each { |d| d.mood = "nervous"}
     pets[:cats].each { |c| c.mood = "nervous"}
     pets[:fishes].each { |f| f.mood = "nervous"}
-    pets.clear
-    #binding.pry
+    pets
+    binding.pry
   end
 
   def list_pets
-    
+
   end
 
 end
